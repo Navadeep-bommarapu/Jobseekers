0ba43f2134182df4ba2e0123403ecb23c042a50a
Bommarapu Navadeep <bommarapunavadeep@gmail.com>
2026-06-13 22:34:56 +0530

    Backend Connection Check

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,11 +1,37 @@
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
 import Footer from "../components/Footer"
+import { useState } from "react"
+import { useEffect } from "react"
+import axios from "axios"
 
 const Landing = () => {
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+    .then((response) => {
+      setJokes(response.data)
+      
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
   return (
     <>
       <Navbar />
+
+      <p>Jokes: {jokes.length}</p>
+      
+      {jokes.map((joke) => {
+        return <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+      })}
+
       <Hero />
       <Footer />
     </>
